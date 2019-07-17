62e1642d4b571ef37b2ff892744e931ae54df5d2
Qian. L <lqian1995@qq.com>
2019-07-17 10:53:09 +0800

    Setup console view.

--- app/src/main/java/cn/edu/whu/unsc/audioinfo/MainActivity.kt
+++ app/src/main/java/cn/edu/whu/unsc/audioinfo/MainActivity.kt
@@ -1,12 +1,23 @@
 package cn.edu.whu.unsc.audioinfo
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tbruyelle.rxpermissions2.RxPermissions
+
+
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initializePermissions()
+    }
+
+    private fun initializePermissions() {
+
     }
 }
