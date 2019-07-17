775c2c67fe61316e13e2254e3dcfc00dceaec824
Qian. L <lqian1995@qq.com>
2019-07-17 11:03:26 +0800

    Add Manifest.permission.RECORD_AUDIO.

--- app/src/main/java/cn/edu/whu/unsc/audioinfo/MainActivity.kt
+++ app/src/main/java/cn/edu/whu/unsc/audioinfo/MainActivity.kt
@@ -1,9 +1,11 @@
 package cn.edu.whu.unsc.audioinfo
 
 import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.tbruyelle.rxpermissions2.RxPermissions
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 
 
@@ -18,6 +20,31 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initializePermissions() {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO)
+            != PackageManager.PERMISSION_GRANTED) {
 
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.RECORD_AUDIO)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.RECORD_AUDIO),
+                    0)
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
     }
 }
